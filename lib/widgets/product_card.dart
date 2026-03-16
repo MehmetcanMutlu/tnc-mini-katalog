25524f753e956a0afd54fd94c8d8d7ff084264b6
Mehmetcan Mutlu <mehmetcanmutlu@users.noreply.github.com>
2026-03-16 15:18:24 +0300

    fix analyzer issues in product_card.dart

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -60,7 +60,7 @@ class ProductCard extends StatelessWidget {
                             borderRadius: BorderRadius.circular(8),
                             boxShadow: [
                               BoxShadow(
-                                  color: Colors.black.withOpacity(0.08),
+                                  color: Colors.black.withValues(alpha: 0.08),
                                   blurRadius: 4)
                             ],
                           ),
